49251747ba5b38876aeaae55b39e0c732ab687dd
Oracle Knots Developer <dev@oracleknots.org>
2026-06-27 00:47:48 -0400

    Update build.sh with toolchain from depends

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 
 echo "==> Configuring build for Oracle Knots..."
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DRDTS_CONSENT=IMPLICIT
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DRDTS_CONSENT=IMPLICIT --toolchain /home/marcano/oracle-knots/depends/x86_64-pc-linux-gnu/toolchain.cmake
 
 echo "==> Compiling Oracle Knots binaries..."
 cmake --build build -j$(nproc)
